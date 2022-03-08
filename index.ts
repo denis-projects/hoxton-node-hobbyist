fb98f44255e91c14418e97f4e4f5d7208f5883c6
besmaerteralb <denis.mimini.1@outlook.com>
2022-03-08 18:45:01 +0100

    working with create a new user

--- index.ts
+++ index.ts
@@ -9,11 +9,34 @@ app.use(express.json())
 
 const prisma = new PrismaClient({ log: ['query', 'error', 'info', 'warn'] });
 
+
+// users
+
 app.get('/users', async (req, res) => {
     const users = await prisma.user.findMany({ include: { hobbies: true } })
     res.send(users)
 })
 
+app.post('/users', async (req, res) => {
+    const { fullName, photo, email, hobbies = [] } = req.body
+    const newUsers = await prisma.user.create({
+        data: {
+            fullName: fullName,
+            photo: photo,
+            email: email,
+            hobbies: {
+                connectOrCreate: hobbies.map((hobby: any) => ({
+                    where: { name: hobby.name },
+                    create: hobby
+                }))
+            }
+        }
+    })
+    res.send(newUsers)
+})
+
+
+// hobbies
 app.get('/hobbies', async (req, res) => {
     const hobbies = await prisma.hobby.findMany({ include: { users: true } })
     res.send(hobbies)
