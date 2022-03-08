1feebf9f22ddd9c6dde8a466120948a0df2b4998
besmaerteralb <denis.mimini.1@outlook.com>
2022-03-08 18:14:24 +0100

    getting all the users and all the hobbies

---
+++ index.ts
@@ -0,0 +1,25 @@
+import express, { response } from "express"
+import { PrismaClient } from "@prisma/client"
+import cors from "cors"
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+const prisma = new PrismaClient({ log: ['query', 'error', 'info', 'warn'] });
+
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany()
+    res.send(users)
+})
+
+app.get('/hobbies', async (req, res) => {
+    const hobbies = await prisma.hobby.findMany()
+    res.send(hobbies)
+})
+
+app.listen(4000, () => {
+    console.log(`Server up: http://localhost:4000`);
+
+})
